413e659a6515e3122a18176cddba874a86b6172c
Tomek_M <9454031marcinkowski@gmail.com>
2019-04-06 14:55:16 +0200

    zrobione dziala ale kod do poprawy

--- 04_smog-tests/src/main/java/foo/bar/smog/PmAlarmService.java
+++ 04_smog-tests/src/main/java/foo/bar/smog/PmAlarmService.java
@@ -7,11 +7,29 @@ public class PmAlarmService {
     public AlarmLevel getAlarmMessage(int measurement, Country country) {
         switch (country) {
             case ITALY:
-                break;
+                if (measurement > 0 && measurement < 50 ) {
+                    return AlarmLevel.NONE;
+                } else if(measurement >= 50 && measurement < 75) {
+                    return AlarmLevel.INFO;
+                } else {
+                    return AlarmLevel.WARNING;
+                }
             case FINLAND:
-                break;
+                if (measurement > 0 && measurement < 50 ) {
+                    return AlarmLevel.NONE;
+                } else if(measurement < 80) {
+                    return AlarmLevel.INFO;
+                } else {
+                    return AlarmLevel.WARNING;
+                }
             case FRANCE:
-                break;
+                if (measurement > 0 && measurement < 80 ) {
+                    return AlarmLevel.NONE;
+                } else if(measurement == 80) {
+                    return AlarmLevel.INFO;
+                } else {
+                    return AlarmLevel.WARNING;
+                }
             case POLAND:
                 if (measurement > 0 && measurement < 200 ) {
                     return AlarmLevel.NONE;
--- 04_smog-tests/src/test/java/foo/bar/smog/PmAlarmServiceTest.java
+++ 04_smog-tests/src/test/java/foo/bar/smog/PmAlarmServiceTest.java
@@ -1,26 +1,53 @@
 package foo.bar.smog;
 
+import foo.bar.fizzbuzz.FizzBuzz;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.logging.Logger;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class PmAlarmServiceTest {
 
+    static PmAlarmService service;
+    private static long timeExecution = 01;
+    static long start =01;
+
+
+    @BeforeAll
+    static void classSetup() {
+        service = new PmAlarmService();
+        start =System.currentTimeMillis();
+    }
+
+    @AfterAll
+    static void afterComplete() {
+        long finish = System.currentTimeMillis();
+        timeExecution=finish-start;
+        Logger.getLogger("JUnit 5").info("Wszystkie testy trwaly " + timeExecution + "ms");
+
+    }
+
+
     @Test
     void shouldReturnNoneForMeasurement_10() {
-        PmAlarmService service = new PmAlarmService();
         assertEquals(AlarmLevel.NONE, service.getAlarmMessage(10, Country.POLAND));
     }
 
     @Test
     void shouldReturnInfoForMeasurement_201() {
-        PmAlarmService service = new PmAlarmService();
+
         assertEquals(AlarmLevel.INFO, service.getAlarmMessage(201, Country.POLAND));
     }
 
     @Test
     void shouldReturnInfoForMeasurement_350() {
-        PmAlarmService service = new PmAlarmService();
         assertEquals(AlarmLevel.WARNING, service.getAlarmMessage(301, Country.POLAND));
     }
+
 }
